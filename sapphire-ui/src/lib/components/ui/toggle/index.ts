db4b77c78f172e498775673646880747b7936277
levish0 <shiueo.csh@gmail.com>
2026-03-27 15:42:47 +0900

    pw component

---
+++ sapphire-ui/src/lib/components/ui/toggle/index.ts
@@ -0,0 +1,13 @@
+import Root from '$lib/components/ui/toggle/toggle.svelte';
+export {
+	toggleVariants,
+	type ToggleSize,
+	type ToggleVariant,
+	type ToggleVariants
+} from '$lib/components/ui/toggle/toggle.svelte';
+
+export {
+	Root,
+	//
+	Root as Toggle
+};
